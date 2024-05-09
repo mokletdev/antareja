4d5d316fb9dfdc8afa932f9ebf3e42f1bce6b1e3
Teguh Bayu Pratama <teguhbayupratama@gmail.com>
2024-05-09 21:35:37 +0700

    feat(frontend): Implement register page

--- src/app/components/global/Navbar.tsx
+++ src/app/components/global/Navbar.tsx
@@ -5,6 +5,8 @@ import Image from "next/image";
 import Link from "next/link";
 import { SecondaryButton } from "./Button";
 import { TertiaryLinkButton } from "./LinkButton";
+import { useSession } from "next-auth/react";
+import { useState } from "react";
 
 interface NavOption {
   label: string;
@@ -21,6 +23,9 @@ const NavOptions: NavOption[] = [
 ];
 
 export default function Navbar() {
+  const [isOpened, setIsOpened] = useState(false);
+  const { data: Session } = useSession();
+
   return (
     <nav className="w-full -mt-[88px] h-[88px] bg-neutral-500 flex items-center justify-between px-[50px] py-[22px] fixed z-[999]">
       <Link href={"/"}>
@@ -37,16 +42,36 @@ export default function Navbar() {
           ))}
         </div>
         <div className="flex gap-3">
-          <SecondaryButton
-            onClick={() => signIn()}
-            type="button"
-            className="font-bold drop-shadow-glow"
-          >
-            Log in
-          </SecondaryButton>
-          <TertiaryLinkButton href="/auth/register" className="font-bold">
-            Sign up
-          </TertiaryLinkButton>
+          {Session ? (
+            <button
+              className="w-[40px] h-[40px] rounded-full overflow-hidden"
+              onClick={() => setIsOpened(!isOpened)}
+            >
+              <Image
+                alt="User"
+                src={
+                  "https://st3.depositphotos.com/6672868/13701/v/450/depositphotos_137014128-stock-illustration-user-profile-icon.jpg"
+                }
+                unoptimized
+                width={40}
+                height={40}
+                className="object-cover w-full h-full"
+              />
+            </button>
+          ) : (
+            <>
+              <SecondaryButton
+                onClick={() => signIn()}
+                type="button"
+                className="font-bold drop-shadow-glow"
+              >
+                Log in
+              </SecondaryButton>
+              <TertiaryLinkButton href="/auth/register" className="font-bold">
+                Sign up
+              </TertiaryLinkButton>
+            </>
+          )}
         </div>
       </div>
     </nav>
