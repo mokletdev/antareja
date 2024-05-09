04f73023201ece36a31cd66e99726bc9dd20f25d
Ahsan <ahsanaz461@gmail.com>
2024-05-09 19:28:36 +0700

    chore: Remove authToken query file

--- src/queries/authToken.query.ts
+++
@@ -1,6 +0,0 @@
-import prisma from "@/lib/prisma";
-
-export async function findToken(userId: string) {
-  const token = await prisma.authToken.findUnique({ where: { userId } });
-  return token;
-}
