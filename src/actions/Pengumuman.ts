28eaa7e0d1296f766e30e176b931397fe1356462
FadhilKholaf <fadhilgd123@gmail.com>
2024-05-13 09:20:14 +0700

    feat(backend): Add pengumuman CRD system

---
+++ src/actions/Pengumuman.ts
@@ -0,0 +1,30 @@
+"use server";
+
+import { revalidatePath } from "next/cache";
+import { createPengumuman, deletePengumuman } from "@/queries/pengumuman.query";
+
+export async function createPengumumanForm(data: FormData) {
+  const content = data.get("content") as string;
+
+  try {
+    await createPengumuman({
+      content: content,
+    });
+    revalidatePath("/admin/pengumuman");
+    return { success: true };
+  } catch (e) {
+    console.log(e);
+    return { success: false };
+  }
+}
+
+export async function deletePengumumanForm(id: string) {
+  try {
+    await deletePengumuman({ id: id });
+    revalidatePath("/admin/pengumuman");
+    return { success: true };
+  } catch (e) {
+    console.log(e);
+    return { success: false };
+  }
+}
