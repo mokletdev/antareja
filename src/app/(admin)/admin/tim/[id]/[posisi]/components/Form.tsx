1cc551b06f6dbafc603370548165e4ed9aa32481
teguhbayu <teguhbayupratama@gmail.com>
2024-09-20 17:11:03 +0800

    feat: Make admin team profile responsive

--- src/app/(admin)/admin/tim/[id]/[posisi]/components/Form.tsx
+++ src/app/(admin)/admin/tim/[id]/[posisi]/components/Form.tsx
@@ -4,6 +4,7 @@ import { PrimaryButton } from "@/app/components/global/Button";
 import { H2, H3, P } from "@/app/components/global/Text";
 import cn from "@/lib/clsx";
 import { Anggota, Kelas } from "@prisma/client";
+import Image from "next/image";
 import { useRouter } from "next/navigation";
 
 const kelas: { label: Kelas; value: Kelas }[] = [
@@ -21,9 +22,18 @@ export default function DisplayAnggota({
   const router = useRouter();
 
   return (
-    <form className="mx-[100px] my-[24px]">
+    <form className="mx-[20px] md:mx-[100px] my-[24px]">
       <H2 className="mb-4">Informasi Anggota</H2>
       <div className="w-full bg-white rounded-lg p-5 flex flex-col gap-8">
+        <div className="w-full flex justify-center items-center">
+          <Image
+            alt={`${anggota.nama}'s photo`}
+            src={anggota.foto}
+            width={600}
+            height={800}
+            className="rounded-xl object-cover w-[40vw] md:w-[15vw] aspect-[2/3]"
+          />
+        </div>
         <div className="flex flex-col gap-1">
           <H3>Nama</H3>
           <P>{anggota.nama}</P>
